da9c0150859fc8439a7e3c748b849fc6733274db
romairi <romairi1991@gmail.com>
2021-12-18 16:45:46 +0200

    creating folder containers

--- library-components/src/containers/App/index.js
+++ library-components/src/containers/App/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./index.scss";
+
 const App = () => {
     return (
         <div>
--- library-components/src/index.js
+++ library-components/src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
 
+import "./index.scss";
+
 ReactDOM.render(<App/>, document.getElementById("root"));
